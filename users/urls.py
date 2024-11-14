5a73f6adccf41ce5ef18f5302bcac5453db3d594
Duncan <84990667+gaidheal1@users.noreply.github.com>
2024-11-14 20:52:28 +0000

    importing extra module

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,11 @@
 # users/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views  # Import Django's auth views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
-
-
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),  # Use auth_views
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),  # Use auth_views
 ]
